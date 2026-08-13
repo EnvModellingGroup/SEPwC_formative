dc051116d3a65538fc179ab100bc2facb4ee838e
Jon Hill <jon.hill@york.ac.uk>
2026-08-13 10:28:59 +0100

    Script written

--- python/todo.py
+++ python/todo.py
@@ -1,3 +1,9 @@
+"""
+todo.py is a simple to-do list application that
+keeps a users to-do list as a text file.
+
+Users can add, list or remove tasks
+"""
 import argparse
 import os
 
@@ -9,15 +15,52 @@ def add_task(task):
     Input - a task to add to the list
     Return - nothing
     """
+    with open(TASK_FILE, "a", encoding="utf-8") as file:
+        file.write(task + "\n")
 
 def list_tasks():
-    return
+    """Function: list_tasks
+
+    Input - a task to add to the list
+    Return - a string of the tasks on the list
+    """
+
+    output_string = ""
+    if os.path.exists(TASK_FILE):
+        with open(TASK_FILE, "r", encoding="utf-8") as file:
+            tasks = file.readlines()
+            for index, task in enumerate(tasks, start=1):
+                output_string += (f"{index}. {task.strip()}\n")
+    return output_string.strip()
+
 
 
 def remove_task(index):
-    return
+    """Function: remove_task
+
+    Input - a task to add to the list
+    Return - nothing
+    """
+
+    if os.path.exists(TASK_FILE):
+        with open(TASK_FILE, "r", encoding="utf-8") as file:
+            tasks = file.readlines()
+        with open(TASK_FILE, "w", encoding="utf-8") as file:
+            for i, task in enumerate(tasks, start=1):
+                if i != index:
+                    file.write(task)
+        print("Task removed.")
+    else:
+        print("No tasks found.")
+
 
 def main():
+    """Function: main
+    
+    Input - nothing
+    Return - nothing
+    """
+
     parser = argparse.ArgumentParser(description="Command-line Todo List")
     parser.add_argument(
             "-a",
